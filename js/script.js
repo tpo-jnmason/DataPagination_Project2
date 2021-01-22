e0b04de1cacc0dca0c0b1d67d407b712c9e51195
tpo-jnmason <76971515+tpo-jnmason@users.noreply.github.com>
2021-01-22 17:01:30 -0500

    working meets

--- js/script.js
+++ js/script.js
@@ -3,16 +3,13 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
  const studentsPerPage = 9
+ const studentList = document.querySelector('.student-list');
+ const header = document.querySelector('header');
+ const linkList = document.querySelector('.link-list');
+
+ // The createElement function allows an easy way to pull the student data from
+ // data.js in order to display to the web page without duplicating code.
 
  function createElement(elementName, property, value) {
    const element = document.createElement(elementName)
@@ -20,21 +17,15 @@ For assistance:
    return element
  }
 
- // Create the `showPage` function
-// This function will create and insert/append the elements needed to display a "page" of nine students
-// */
+
+// The showPage function creates the elements needed to display a "page" of nine students
+// The loop is needed in order to create all student profiles.
+
  function showPage(list, page) {
-  // create two variables which will represent the index for the first and last student on the page
      const startIndex = (page * studentsPerPage) - studentsPerPage;
      const endIndex = page * studentsPerPage;   
-     const studentList = document.querySelector('.student-list');
-// //     //set the innerHTML property of the variable you just created to an empty string
      studentList.innerHTML = '';
-    // loop over the length of the `list` parameter
-    // inside the loop create a conditional to display the proper students
-        // inside the conditional:
-          // create the elements needed to display the student information
-          // insert the above elements
+
      for (let i = 0; i < list.length; i++) {
          if ( i >= startIndex && i < endIndex) {
             const li = createElement('li', 'className', 'student-item cf');
@@ -66,45 +57,35 @@ For assistance:
          }
       }
  }
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+
+//The addPagination function creates the elements needed for the pagination buttons to 
+//allow direct page navigation. The loop is necessary to create the appropriate number 
+//of pages
 
 function addPagination(list) {
-   // create a variable to calculate the number of pages needed
     let numOfPages = Math.ceil(list.length/studentsPerPage);
- // select the element with a class of `link-list` and assign it to a variable
-    const linkList = document.querySelector('.link-list');
-   // set the innerHTML property of the variable you just created to an empty string
     linkList.innerHTML = ''
-   // loop over the number of pages needed
-   for (let i = 1; i <= numOfPages; i++ ){
-       const button = createElement('button', 'textContent', `${i}`);
-       button.type = 'button';
-       linkList.appendChild(button);
+
+    for (let i = 1; i <= numOfPages; i++ ){
+        const buttonList= createElement('li', '', '');
+        linkList.appendChild(buttonList);
+        const button = createElement('button', 'textContent', `${i}`);
+        button.type = 'button';
+        buttonList.appendChild(button);
    } 
-     // create the elements needed to display the pagination button
-     // insert the above elements
-   // give the first pagination button a class of "active"
+        let activeButton = linkList.firstElementChild.firstElementChild;
+        activeButton.className = 'active';
 
-   linkList.addEventListener('click', (e) => {
-      let activeButton = linkList.querySelector('button');
-      activeButton.className = 'active';
+    linkList.addEventListener('click', (e) => {
 
-      if (e.target.type === 'button') {
-         let page = e.target.textContent;
-         linkList.querySelector('.active').className = '';
-         e.target.className = 'active';
-         showPage(list, page);
-      }
-   });
+        if (e.target.type === 'button') {
+            let page = e.target.textContent;
+            linkList.querySelector('.active').className = '';
+            e.target.className = 'active';
+            showPage(list, page);
+        }
+    });
 }
-    // create an event listener on the `link-list` element
-     // if the click target is a button:
-       // remove the "active" class from the previous button
-       // add the active class to the clicked button
-       // call the showPage function passing the `list` parameter and page to display as arguments
-// Call functions
+
 showPage(data,1);
 addPagination(data);
